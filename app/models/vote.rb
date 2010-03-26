18821056f4de7bd7c787c551e413fe436f5eec1d
Nathan Reeves <nathanreeves@Nathan-Reevess-MacBook-Pro.local>
2010-03-26 21:10:49 +1100

    postgres group by

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,8 +9,8 @@ class Vote < ActiveRecord::Base
   #named_scope :popular_stories, :group => 'story_id,social_tag_id,story_tag_id,sport_id,id,sentiment,created_at,updated_at', :order=> "created_at DESC", :limit=>10
   #named_scope :popular_tags, :group => 'social_tag_id,story_id,story_tag_id,sport_id,id,sentiment,created_at,updated_at' , :order=> "created_at DESC" ,:limit=>10
   
-  named_scope :popular_stories,:select => 'DISTINCT (story_id),created_at,id',  :order=> "created_at DESC", :limit=>10
-  named_scope :popular_tags, :select => 'DISTINCT (social_tag_id),created_at,id',  :order=> "created_at DESC" ,:limit=>10
+  named_scope :popular_stories,:select => 'DISTINCT ON (story_id),story_id,created_at,id',  :order=> "created_at DESC", :limit=>10
+  named_scope :popular_tags, :select => 'DISTINCT ON (social_tag_id),social_tag_id,created_at,id',  :order=> "created_at DESC" ,:limit=>10
   
   
   
